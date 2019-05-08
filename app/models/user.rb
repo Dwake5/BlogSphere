5bfa042562331d0ecc5398481cf471ce1a63ea95
github username <github email address>
2019-05-08 19:02:51 +0100

    Added models

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+    has_many :user_posts
+    has_many :posts, through: :user_posts
+
+    validates :name, :email, :password_digest, presence: true
+    validates :email, uniqueness: true
+    has_secure_password
+end
